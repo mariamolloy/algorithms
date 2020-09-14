232d7749b7b97211b45ea1882599061fce2b7108
Maria <mariamango3@gmail.com>
2020-09-14 17:20:24 -0400

    jk fixed push

--- 1lab/src/pack/elemDataStructs/Stack.java
+++ 1lab/src/pack/elemDataStructs/Stack.java
@@ -18,10 +18,21 @@ public class Stack {
     }
   }
 
+  //method to add an element to the top of the stack
+  //element = the String data to be added to the stack
   public void push(String element){
+
+    //create a new node w the data we want to add to the stack
     Node myNode = new Node(element);
-    Node prevTop = this.top;
+
+    //set the new node's pointer to the node that used to be at the top of the stack
+    myNode.pointer = this.top;
+
+    //set the top to the node we just added
     this.top = myNode;
-    myNode.pointer = prevTop;
+  }
+
+  public void pop(){
+
   }
 }
