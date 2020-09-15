3bcd34706362967fc563c8771dd22fa2d85fcb89
Maria <mariamango3@gmail.com>
2020-09-15 02:39:01 -0400

    got basic idea of a palindrome method down

--- 1lab/src/pack/elemDataStructs/Palindrome.java
+++ 1lab/src/pack/elemDataStructs/Palindrome.java
@@ -1,5 +1,67 @@
 package pack.elemDataStructs;
 
+
 public class Palindrome {
+  Node word;
+
+  Palindrome(Node pal){
+    word = pal;
+  }
+
+  //goes through a list to check for palindromes
+  public static boolean isPalindrome(LinkedList l){
+    //traverse da list
+    Node currentNode = l.head;
+    while (currentNode != null){
+
+      //get data of current node
+      String str = currentNode.data;
+      Stack wordS = new Stack();
+      Queue wordQ = new Queue();
+
+      //counter to make sure each set of letters in word matches
+      //only works rn for even numbers
+      int palCheck = 0;
+
+      //go through word and add each letter to stack and a queue
+      for (int i = 0; i < str.length(); i++) {
+
+        //realllllly its gonna be a char every time but chars are more annoying than strings imho
+        String letter = "";
+
+        //only add one letter but check so no substring out of bounds error
+        if (i == (str.length() - 1)){
+          letter = str.substring(str.length() - 1);
+        } else {
+          letter = str.substring(i, i + 1);
+        }
+        //add each letter to stack/queue
+        wordS.push(letter);
+        wordQ.queue(letter);
+      }
+
+      //go thru our stack and queue and pop/dequeue to test letters against each other to find palindromes
+      //this only works for even number amt of lettered words rn i think
+      for (int j = str.length(); j > 0; j--){
+
+        //just for testing pur poses
+        System.out.print(wordS.pop().data);
+        //if both letters returned are the same, increase palCheck (pal is checkin out well)
+        if (wordS.pop().data.equals(wordQ.dequeue().data)){
+          palCheck++;
+        }
+      }
+
+      //if palCheck == str.length then it is a palindrome!!! yayyyy
+      if (palCheck == str.length()){
+        return true;
+      }else {
+        return false;
+      }
+
+      currentNode = currentNode.pointer;
+    }
+  }
+
 
 }
--- 1lab/src/pack/elemDataStructs/Queue.java
+++ 1lab/src/pack/elemDataStructs/Queue.java
@@ -2,4 +2,19 @@ package pack.elemDataStructs;
 
 public class Queue {
 
+  //create a head of the queue
+  Node head;
+
+  //queue constructor
+  Queue(){
+    this.head = null;
+  }
+
+  public static void queue(String l){
+
+  }
+  public static Node dequeue(){
+
+  }
+
 }
