617325d9a177ad9c83821a7767c5f19795388492
mariamolloy <35545523+mariamolloy@users.noreply.github.com>
2020-12-10 15:35:51 -0500

    almosttttt done

--- final project/src/Simulation.java
+++ final project/src/Simulation.java
@@ -6,12 +6,22 @@ public class Simulation {
     public double infectionRate;
     public double testAccuracy;
     public int groupSize;
-    int testsUsed = 0;
+
+    int totalTestsUsed = 0; //total tests we use
+    int testCounter = 0;
+    int totalSick = 0; //total amount of infected people we find //should = popSize * infectionRate (20 in this case)
 
 
+    //counter to keep track of how many instances of each case in a sim run
     int case1Count = 0;
     int case2Count = 0;
     int case3Count = 0;
+
+    //counter to keep track of how many tests are used per case
+    //when sim is done the sum of these three ints should = totalTestsUsed
+    int case1Tests = 0;
+    int case2Tests = 0;
+    int case3Tests = 0;
 
     String print = "";
 
@@ -101,89 +111,106 @@ public class Simulation {
 
         //each time we run through this we make a new group
         //we shld run through this 125 times
-        while((population.isEmpty()) && (groupCounter <= numOfGroups)){
+        while(!(population.isEmpty()) && (groupCounter <= numOfGroups)){
             boolean case2or3 = false; //lets assume its case 1. it probably is
+            this.testCounter = 0; //reset
+
             populationList groupX = new populationList();
 
             for (int i = 0; i < groupSize; i++) {
 
                 Person copyPerson = population.dequeue(); //it feels weird to say new when i named the variable person??
-                this.testsUsed++;
 
                 if (copyPerson.infected == true){
                     //we must find them!!
                     case2or3 = true;
+                    this.testCounter++;
+
                 }
                 groupX.enqueue(copyPerson.infected);
 
             }
-            if (case2or3 == true){ //if its case 1 we dont have to test anymore, no one has it
-                test(true, groupX); //where we do the real work
-            } else {
-                case1Count++;
-            }
+
+            switch(this.testCounter){
+                case 0:
+                    this.case1Count++;
+                    break;
+                case 1:
+                    this.case2Count++;
+                    break;
+                case 3:
+                    this.case3Count++;
+                    break;
             }
 
-            case2Count = case1Count - case3Count;
-            print = (("Case(1): ") + numOfGroups + " x 0.85 -->" + " with " + case1Count + " tests used \n" +
-                     "Case(2): " + numOfGroups + " x 0.1496 -->" +  " with " + case2Count + " tests used \n" +
-                   " Case(3): " + numOfGroups + " x 0.0004 -->" + " with " + case3Count + " tests used \n" +
+            this.test(case2or3, groupX, (testCounter + 1));
+        }
+        if (case1Tests + case2Tests + case3Tests != totalTestsUsed){
+            System.out.println("uh oh we messed up with the test count");
+        }
+            print = (("Case(1): ") + numOfGroups + " x 0.85 --> " +  case1Count + " instances with "+ case1Tests +" tests used \n" +
+                     "Case(2): " + numOfGroups + " x 0.1496 --> " +  case2Count + " instances with " + case2Tests +" tests used \n" +
+                   " Case(3): " + numOfGroups + " x 0.0004 --> " + case3Count + " instances with " + case3Tests + " tests used \n" +
                     "---------------------------------------------------------------------------------------------------------------------------------------------------- \n" +
-                    this.testsUsed + " tests for " + popSize + " people with an infection rate of " + (infectionRate * 100) + "% \n\n\n" +
+                    this.totalTestsUsed + " tests found " + totalSick + " infected people in a population of " + popSize + " people with an infection rate of " + (infectionRate * 100) + "% \n\n\n" +
                     "xoxo Gossip girl");
-
-return print;    }
+            return print;
+    }
 
     //this is where we recurse
-    public void test(boolean inf, populationList exposed){
+    public void test(boolean inf, populationList exposed, int caseNum){
+        this.totalTestsUsed++;
+        switch(caseNum){
+            case 1:
+                this.case1Tests++;
+                break;
+            case 2:
+                this.case2Tests++;
+                break;
+            case 3:
+                this.case3Tests++;
+                break;
+        }
         if (inf == true){
+            this.totalTestsUsed++;
             populationList a = new populationList();
             boolean aInf = false;
-            int howManyMoreTests = 0;
+
             populationList b = new populationList();
             boolean bInf = false;
 
             int grpSz = exposed.getSize(exposed);
-            if (grpSz == 1){
-                if(exposed.first.infected == true){
-                    this.testsUsed++;
+
+            if (grpSz == 2){
+                for (int i = 0; i < 2;i ++){
+                    Person temp = exposed.dequeue();
+                    if (temp.infected == true){
+                        this.totalSick++; //we found the person making the group test positive
+                    }
                 }
             } else {
-                for (int i = 0; i < grpSz; i++){
+                //go through list half by half
+                for (int i = 0; i < grpSz / 2; i++){
                     Person temp = exposed.dequeue(); //this just feels like this the office
-
                     if (temp.infected == true){
-                        if (i < (i/2)){
-                            a.enqueue(temp.infected);
-                            aInf = true;
-                        } else {
-                            b.enqueue(temp.infected);
-                            bInf = true;
-                        }
-                    } else {
-                        if (i < (i/2)){
-                            a.enqueue(temp.infected);
-                        } else {
-                            b.enqueue(temp.infected);
-                        }
+                        aInf = true;
                     }
-
+                    a.enqueue(temp.infected);
+                }
+                for (int j = 0; j < grpSz / 2; j++){
+                    Person temp = exposed.dequeue(); //this just feels like this the office
+                    if (temp.infected == true){
+                        bInf = true;
+                    }
+                    b.enqueue(temp.infected);
                 }
 
+                //no matter what just test both bc
+                test(aInf, a, caseNum);
+                test(bInf, b, caseNum);
             }
 
-            if ((aInf == true) && (bInf == true)) {
-                case3Count++;
-                test(true, a);
-                test(true, b);
-            }else if (aInf == true){
-                case3Count++;
-                test(true, a);
-            } else if (bInf == true){
-                case3Count++;
-                test(true, b);
-            }
-        }
+        } //else : do nothing no infection = were done
     }
 
     //Runs the simulation
